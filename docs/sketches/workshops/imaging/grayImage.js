1eefd17e7b22b4c4a2fcc6b782ad4e6756f76f09
jdacostabe <jdacostabe@unal.edu.co>
2021-03-10 11:18:49 -0500

    Pixel Filters added

--- docs/sketches/workshops/imaging/grayImage.js
+++ docs/sketches/workshops/imaging/grayImage.js
@@ -8,5 +8,22 @@ function setup() {
     createCanvas(800, 550);
     img.resize(800, 550);
     image(img, 0, 0);
-    filter(GRAY);
+
+    //grey scale (rgb) -> ((r+g+b)/3, (r+g+b)/3, (r+g+b)/3)
+
+    let d = pixelDensity();
+    let npixels = 4 * (width * d) * (height * d);
+    loadPixels();
+    for (let i = 0; i < npixels; i += 4) {
+
+        // let gray = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+        let gray = pixels[i] * 0.2126 + pixels[i + 1] * 0.7152 + pixels[i + 2] * 0.0722;
+
+        pixels[i] = gray;
+        pixels[i + 1] = gray;
+        pixels[i + 2] = gray;
+        pixels[i + 3] = pixels[i + 3];
+    }
+    updatePixels();
+    //filter(GRAY);
 }
--- docs/sketches/workshops/imaging/negativeImage.js
+++ docs/sketches/workshops/imaging/negativeImage.js
@@ -8,5 +8,18 @@ function setup() {
     createCanvas(800, 550);
     img.resize(800, 550);
     image(img, 0, 0);
-    filter(INVERT);
+
+    //negative (rgb) -> (255-r,255-g,255-b)
+
+    let d = pixelDensity();
+    let npixels = 4 * (width * d) * (height * d);
+    loadPixels();
+    for (let i = 0; i < npixels; i += 4) {
+
+        pixels[i] = 255 - pixels[i];
+        pixels[i + 1] = 255 - pixels[i + 1];
+        pixels[i + 2] = 255 - pixels[i + 2];
+    }
+    updatePixels();
+    //filter(INVERT);
 }
